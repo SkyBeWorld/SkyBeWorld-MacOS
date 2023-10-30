4c4d5a04663d7efd7341a8e6486a5f050384f4ca
GeantW0rld <yanis.geantworld@gmail.com>
2023-10-30 13:31:10 +0100

    Initial commit

---
+++ SkyBeWorld/Views/FormsView.swift
@@ -0,0 +1,18 @@
+//
+//  FormsView.swift
+//  SkyBeWorld
+//
+//  Created by yanis on 30/10/2023.
+//
+
+import SwiftUI
+
+struct FormsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FormsView()
+}
---
+++ SkyBeWorld/Views/homeView.swift
@@ -0,0 +1,18 @@
+//
+//  homeView.swift
+//  SkyBeWorld
+//
+//  Created by yanis on 30/10/2023.
+//
+
+import SwiftUI
+
+struct homeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    homeView()
+}
