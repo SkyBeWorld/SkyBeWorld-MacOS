ddd5d733f6ff478a9dd90e9b40109dcdf7b3c8fc
GeantW0rld <yanis.geantworld@gmail.com>
2023-10-30 13:08:15 +0100

    Initial Commit

---
+++ SkyBeWorld/SkyBeWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyBeWorldApp.swift
+//  SkyBeWorld
+//
+//  Created by yanis on 30/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SkyBeWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
